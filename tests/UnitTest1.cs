5c344bb619d0dbeec8cf36ecf6422b0741902de8
David <david@evason.co.za>
2017-11-19 12:10:24 +0200

    Better unit tests with Random numbers

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calculator.Domain;
+using System;
 
 namespace tests
 {
@@ -7,16 +8,80 @@ namespace tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddition()
         {
+            var Random = new System.Random();
+            double Answer = 0.0;
+
             var Calculator = new Calculator.Domain.Calculator();
-            Calculator.Current = 3;
+            Calculator.Current = Random.Next();
+            Answer += Calculator.Current;
             Calculator.ProcessOperation(new CalculationOperation(new Add()));
-            Calculator.Current = 4;
+
+            Calculator.Current = Random.Next();
+            Answer += Calculator.Current;
             Calculator.ProcessOperation(new EqualsOperation());
 
-            Assert.AreEqual(7.0, Calculator.Current);
+            Assert.AreEqual(Answer, Calculator.Current);
 
         }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            var Random = new System.Random();
+            double Answer = 0.0;
+
+            var Calculator = new Calculator.Domain.Calculator();
+            Calculator.Current = Random.Next();
+            Answer += Calculator.Current;
+            Calculator.ProcessOperation(new CalculationOperation(new Subtract()));
+
+            Calculator.Current = Random.Next();
+            Answer -= Calculator.Current;
+            Calculator.ProcessOperation(new EqualsOperation());
+
+            Assert.AreEqual(Answer, Calculator.Current);
+
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var Random = new System.Random();
+            double Answer = 0.0;
+
+            var Calculator = new Calculator.Domain.Calculator();
+            Calculator.Current = Random.Next();
+            Answer += Calculator.Current;
+            Calculator.ProcessOperation(new CalculationOperation(new Multiply()));
+
+            Calculator.Current = Random.Next();
+            Answer *= Calculator.Current;
+            Calculator.ProcessOperation(new EqualsOperation());
+
+            Assert.AreEqual(Answer, Calculator.Current);
+
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            var Random = new System.Random();
+            double Answer = 0.0;
+
+            var Calculator = new Calculator.Domain.Calculator();
+            Calculator.Current = Random.Next();
+            Answer += Calculator.Current;
+            Calculator.ProcessOperation(new CalculationOperation(new Divide()));
+
+            Calculator.Current = Random.Next();
+            Answer /= Calculator.Current;
+            Calculator.ProcessOperation(new EqualsOperation());
+
+            Assert.AreEqual(Answer, Calculator.Current);
+
+        }
+
     }
 }
